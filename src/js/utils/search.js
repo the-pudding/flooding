78b5221dd7d0dae3fb4c56c15b1bdd5fa4a23a6b
Amber Thomas <ProQuestionAsker@gmail.com>
2020-06-18 17:28:16 -0700

    text update working on type

--- src/js/utils/search.js
+++ src/js/utils/search.js
@@ -1,15 +1,31 @@
 // these functions are modeled heavily off of Adam Silver's "Building an Accessible Autocomplete Control"
 // https://adamsilver.io/articles/building-an-accessible-autocomplete-control/
 
+let DATA = [];
+let SEARCH = null;
+let TYPE = null;
+let $ac = null;
+
 function handleDownPress(key) {
   console.log('down arrow pressed');
 }
 
-function handleType(key) {
-  console.log('typing');
+function handleType(key, textbox, search, type) {
+  // only show options if user typed something
+  const typed = textbox.node().value;
+  if (typed.trim().length > 1) {
+    // get options based on value
+    const text = typed.toLowerCase();
+    const re = new RegExp(`\\b${text}`);
+
+    const results = DATA.filter((d) => d.location.toLowerCase().match(re));
+
+    // update list of results
+    buildMenu(results, search, type);
+  }
 }
 
-function handleTextInput() {
+function handleTextInput(search, type, textbox) {
   const key = d3.event.code;
 
   // make array of keys to ignore
@@ -26,29 +42,60 @@ function handleTextInput() {
   ];
 
   // figure out what to do based on which key was pressed
-  if (!ignore.includes(key) && key !== 'ArrowDown') handleType(key);
+  if (!ignore.includes(key) && key !== 'ArrowDown')
+    handleType(key, textbox, search, type);
   else if (key === 'ArrowDown') handleDownPress();
 }
 
-function setupDOM({ data, searchBoxSel, locationType }) {
+function buildMenu(data, search, type) {
+  const uniqueID = search.attr('data-js');
+
+  // add li elements on data update
+  $ac
+    .select('ul')
+    .selectAll('li')
+    .data((d) => {
+      return data;
+    })
+    .join((enter) =>
+      enter
+        .append('li')
+        // lets users know this is one option in the list
+        .attr('role', 'option')
+        // focus can be set to the option programmatically
+        .attr('tabindex', '-1')
+        // lets users know which option is selected
+        .attr('aria-selected', 'false')
+        .attr('id', (d, i) => `${uniqueID}-autocomplete_${i}`)
+    )
+    .text((d) => {
+      return type === 'county'
+        ? `${d.location}, ${d.state.toUpperCase()}`
+        : `${d.location}`;
+    })
+    // stores the select box option value
+    .attr('data-option-value', (d, i) => i + 1);
+}
+
+function setupDOM(data, search, type) {
   // add options to the select box for full a11y
-  searchBoxSel
+  search
     .select('select')
     .selectAll('option')
     .data(data)
     .join((enter) => enter.append('option'))
     .text((d) =>
-      locationType === 'county'
+      type === 'county'
         ? `${d.location}, ${d.state.toUpperCase()}`
         : `${d.location}`
     );
 
-  const uniqueID = searchBoxSel.attr('data-js');
+  const uniqueID = search.attr('data-js');
 
   // add autocomplete div
-  const $ac = searchBoxSel
+  $ac = search
     .selectAll('.autocomplete')
-    .data([data])
+    .data([DATA])
     .join((enter) => {
       const $parent = enter.append('div').attr('class', 'autocomplete');
 
@@ -84,41 +131,25 @@ function setupDOM({ data, searchBoxSel, locationType }) {
         .attr('aria-live', 'polite')
         .attr('role', 'status')
         // this feedback is only useful to screen reader users so it's hidden
-        .attr('class', 'visually-hidden');
+        .attr('class', 'visually-hidden result-count');
 
       return $parent;
     });
 
-  // add li elements on data update
-  $ac
-    .select('ul')
-    .selectAll('li')
-    .data((d) => d)
-    .join((enter) =>
-      enter
-        .append('li')
-        // lets users know this is one option in the list
-        .attr('role', 'option')
-        // focus can be set to the option programmatically
-        .attr('tabindex', '-1')
-        // lets users know which option is selected
-        .attr('aria-selected', 'false')
-        .attr('id', (d, i) => `${uniqueID}-autocomplete_${i}`)
-    )
-    .text((d) =>
-      locationType === 'county'
-        ? `${d.location}, ${d.state.toUpperCase()}`
-        : `${d.location}`
-    )
-    // stores the select box option value
-    .attr('data-option-value', (d, i) => i + 1);
+  buildMenu(data, search, type);
 
   // add type event listener
-  $ac.on('keyup', handleTextInput);
+  $ac.on('keyup', function (d) {
+    const textbox = d3.select(this).select('input');
+    handleTextInput(search, type, textbox);
+  });
 }
 
 export default function setupSearch(data, searchBoxSel, locationType) {
   return new Promise((resolve, reject) => {
-    setupDOM({ data, searchBoxSel, locationType });
+    setupDOM(data, searchBoxSel, locationType);
+    DATA = data;
+    SEARCH = searchBoxSel;
+    TYPE = locationType;
   });
 }
